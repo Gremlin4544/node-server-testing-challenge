0ef649dbfbeab35ee24c946cad16f62e782777a1
Gremlin4544 <alve@mckethen.com>
2020-03-26 15:52:06 -0400

    updated server file with endpoints

--- api/server.js
+++ api/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 
+const Cities = require('../cities/cities-models.js');
+
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.send("Qui Gon - its working! Its working");
+  res.status(200).json({message: "Qui Gon - its working! Its working"});
+});
+
+server.get('/cities', (req, res) => {
+  Cities.getAll()
+  .then(cities => {
+    res.status(200).json(cities);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = server;
