fb4f0cead2f00ac806f5cb005a053a5c00726352
Gremlin4544 <alve@mckethen.com>
2020-03-26 15:31:59 -0400

    ran migrate latest

--- migrations/20200326152431_cities.js
+++ migrations/20200326152431_cities.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
     return knex.schema
     .createTable('cities', tbl => {
-        tbl.increment();
+        tbl.increments();
 
         tbl
         .string('city', 128)
