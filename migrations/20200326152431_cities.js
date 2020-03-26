48cd34b8cf3d6dc40f9179bb96a15419fde8b3ac
Gremlin4544 <alve@mckethen.com>
2020-03-26 15:29:03 -0400

    updated cities file

--- migrations/20200326152431_cities.js
+++ migrations/20200326152431_cities.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('cities', tbl => {
+        tbl.increment();
+
+        tbl
+        .string('city', 128)
+        .notNullable()
+        .unique();
+    });
 };
 
 exports.down = function(knex) {
-  
-};
+return knex.schema.dropTableIfExists('cities');
+};  
