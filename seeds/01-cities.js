a5c328153f67cdb9a9a3683c8ed1b265dc2f6ccf
Gremlin4544 <alve@mckethen.com>
2020-03-26 15:36:46 -0400

    updated 01 cities file

--- seeds/01-cities.js
+++ seeds/01-cities.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cities').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('cities').insert([
+        {city: 'Boulder'},
+        {city: 'Denver'},
+        {city: 'Portland'},
+        {city: 'San Francisco'}
       ]);
     });
 };
