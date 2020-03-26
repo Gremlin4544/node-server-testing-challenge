9a17facebe1d3feed83444a4b3a9243ec0464d55
Gremlin4544 <alve@mckethen.com>
2020-03-26 16:12:22 -0400

    updated knexfile for production

--- knexfile.js
+++ knexfile.js
@@ -21,36 +21,33 @@ module.exports = {
       },
   },
 
-  staging: {
-    client: 'postgresql',
+  testing: {
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/cities.db3'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './migrations'
+    },
+    seeds: {
+      directory: "./seeds",
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './migrations'
+    },
+    seeds: {
+      directory: "./seeds",
+    },
   }
 
 };
